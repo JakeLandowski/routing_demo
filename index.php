d56d171a9c09eb1deb5f284abd22f6d868ef35c6
JakeLandowski <jakelandowski@gmail.com>
2018-01-16 14:30:36 -0800

    init commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require_once 'vendor/autoload.php';
+
+$f3 = Base::instance();
+
+$f3->set('DEBUG', 3);
+
+$f3->route('GET', function(){
+    echo '<h1>Routing Demo</h1>';
+});
+
+$f3->run();
